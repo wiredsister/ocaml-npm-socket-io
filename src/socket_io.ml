1d4e84e9fe9a9fd29a81321debc75530478f8e92
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-10-16 00:33:17 -0700

    Update build crap, expose more methods

--- src/socket_io.ml
+++ src/socket_io.ml
@@ -1,8 +1,9 @@
 
 
 class type socket_io = object
-
+  inherit Nodejs.Events.event_emitter
+  method listen : Nodejs.Http.server -> socket_io Js.t Js.meth
 end
 
 let require () : socket_io Js.t =
-  Internal.require (Js.string "socket.io")
+  Nodejs_globals.require (Js.string "socket.io")
