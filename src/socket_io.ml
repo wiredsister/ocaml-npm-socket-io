a400e166c700122e6e770db006698579da995909
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-10-17 00:21:01 -0700

    Remove crappy event_emmiter object

--- src/socket_io.ml
+++ src/socket_io.ml
@@ -3,7 +3,6 @@ class type client = object
 end
 
 class type socket = object
-  inherit Nodejs.Events.event_emitter
   method client : client Js.t Js.readonly_prop
   method on :
     Js.js_string Js.t ->
@@ -12,18 +11,21 @@ class type socket = object
 end
 
 class type namespace = object
-  inherit Nodejs.Events.event_emitter
-  method on_ :
+  method emit :
+    Js.js_string Js.t ->
+    (Js.Unsafe.any -> unit) Js.callback ->
+    unit Js.meth
+
+  method on :
     Js.js_string Js.t ->
     (socket Js.t -> unit) Js.callback ->
     unit Js.meth
 end
 
 class type socket_io = object
-  inherit Nodejs.Events.event_emitter
   method listen : Nodejs.Http.server Js.t -> socket_io Js.t Js.meth
   method sockets : namespace Js.t Js.readonly_prop
-  method emit_ :
+  method emit :
     Js.js_string Js.t ->
     Js.Unsafe.any ->
     unit Js.meth
