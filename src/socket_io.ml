d340be7a9bb6bdb57e2e7a781eb19bfc3ea2c7b5
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-10-21 10:06:38 -0700

    Fix require

--- src/socket_io.ml
+++ src/socket_io.ml
@@ -13,7 +13,7 @@ end
 class type namespace = object
   method emit :
     Js.js_string Js.t ->
-    (Js.Unsafe.any -> unit) Js.callback ->
+    ('a Js.t -> unit) Js.callback ->
     unit Js.meth
 
   method on :
@@ -22,21 +22,27 @@ class type namespace = object
     unit Js.meth
 end
 
+class type server = object
+
+  method on :
+    Js.js_string Js.t ->
+    (socket Js.t -> unit) Js.callback ->
+    unit Js.meth
+
+end
+
 class type socket_io = object
   method listen : Nodejs.Http.server Js.t -> socket_io Js.t Js.meth
   method sockets : namespace Js.t Js.readonly_prop
+  method call_arg : 'a Js.t -> server Js.t Js.meth
   method emit :
     Js.js_string Js.t ->
     Js.Unsafe.any ->
     unit Js.meth
 end
 
-class type server = object
-
-end
-
 let require () : socket_io Js.t =
-  Nodejs_globals.require (Js.string "socket.io")
+  Nodejs_globals.require "socket.io"
 
 let server () : server Js.t =
   Js.Unsafe.eval_string "require(\"socket.io\")()"
