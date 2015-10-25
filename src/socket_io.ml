9520ad8f460f239c0adfb61be8b29dd0974406fd
Edgar Aroutiounian <edgar.factorial@gmail.com>
2015-10-25 11:45:20 -0700

    Update Changes from upstream Node package

--- src/socket_io.ml
+++ src/socket_io.ml
@@ -1,48 +1,56 @@
+open Nodejs_kit
+
 class type client = object
   (* method conn :  *)
 end
 
+(** A socket object, not the module *)
 class type socket = object
+
+  method username : js_str Js.readonly_prop
+
   method client : client Js.t Js.readonly_prop
-  method on :
-    Js.js_string Js.t ->
-    (Js.Unsafe.any -> unit) Js.callback ->
+
+  method broadcast : socket Js.t Js.readonly_prop
+
+  method emit :
+    js_str ->
+    <username : js_str; message : js_str> Js.t ->
     unit Js.meth
+
+  method on : js_str -> (Js.Unsafe.any Js.t -> unit) Js.callback -> unit Js.meth
+
 end
 
 class type namespace = object
-  method emit :
-    Js.js_string Js.t ->
-    ('a Js.t -> unit) Js.callback ->
-    unit Js.meth
 
-  method on :
-    Js.js_string Js.t ->
-    (socket Js.t -> unit) Js.callback ->
-    unit Js.meth
+  method emit : js_str -> ('a Js.t -> unit) Js.callback -> unit Js.meth
+
+  method on : js_str -> (socket Js.t -> unit) Js.callback -> unit Js.meth
+
 end
 
 class type server = object
 
-  method on :
-    Js.js_string Js.t ->
-    (socket Js.t -> unit) Js.callback ->
-    unit Js.meth
+  method on : js_str -> (socket Js.t -> unit) Js.callback -> unit Js.meth
 
 end
 
+let server : (unit -> server Js.t) Js.constr =
+  Js.Unsafe.js_expr "require(\"socket.io\")"
+
+(** The module object socket.io *)
 class type socket_io = object
+
   method listen : Nodejs.Http.server Js.t -> socket_io Js.t Js.meth
+
   method sockets : namespace Js.t Js.readonly_prop
+
   method call_arg : 'a Js.t -> server Js.t Js.meth
-  method emit :
-    Js.js_string Js.t ->
-    Js.Unsafe.any ->
-    unit Js.meth
+
+  method emit : js_str -> 'a Js.t -> unit Js.meth
+
 end
 
 let require () : socket_io Js.t =
-  Nodejs_globals.require "socket.io"
-
-let server () : server Js.t =
-  Js.Unsafe.eval_string "require(\"socket.io\")()"
+  Nodejs_kit.require "socket.io"
